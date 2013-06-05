749bf0a9f4c12ebb75218c48b32be85746b38115
Thomas Levine <occurrence@thomaslevine.com>
2013-06-04 18:50:09 -0700

    handle errors

--- app.py
+++ app.py
@@ -6,21 +6,31 @@ from flask import Flask
 from flask import Response
 
 app = Flask(__name__)
+# SEED = 2958073676721306211
 SEED = ##seed##
 CANONICAL = 'http://appgen.me/a/'
 
 querystring = '?seed=' + unicode(SEED)
 
+@app.route('/favicon.ico')
+def favicon():
+    return Response(status = 404)
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def proxy(path):
     url = CANONICAL + path + querystring
-    f = urllib2.urlopen(url)
-    response = f.read().replace(querystring, '').replace('"/a', '"') # remove seed and /a
-    status = f.getcode()
-    headers = f.headers.dict
-    content_type = headers.get('content-type', 'text/html')
-    return Response(response=response, status=status, headers=headers, content_type=content_type)
+    try:
+        f = urllib2.urlopen(url)
+        response = f.read().replace(querystring, '') # remove seed
+        status = f.getcode()
+        headers = f.headers.dict
+        content_type = headers.get('content-type', 'text/html')
+        return Response(response=response, status=status, headers=headers, content_type=content_type)
+    except Exception, e:
+        print url
+        print e.msg
+        return Response(status = 404)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
